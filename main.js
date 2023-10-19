3f9df8a3c943a1cec13227bcddc3634f9754a156
mey <meydetour@gmail.com>
2023-10-20 01:20:56 +0200

    s

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 const min = document.querySelector(".min")
 const hour = document.querySelector(".hour")
 const sec = document.querySelector(".sec")
+const heureactuelle = document.querySelector(".heureactuelle")
 
 
 setInterval(()=>{
-    console.log(new Date().getHours(), new Date().getMinutes(), new Date().getSeconds())
-    hour.style.transform = 'rotate('+(-90 + (new Date().getMinutes()-1)*30 + new Date().getMinutes()/60 * 30)+'deg)'
+    heureactuelle.innerHTML = String(new Date().getHours()) +" : " + new Date().getMinutes()+" : " +new Date().getSeconds()
+    hour.style.transform = 'rotate('+(-90 + (new Date().getHours())*30 + new Date().getMinutes()/60 * 30)+'deg)'
     min.style.transform = 'rotate('+(-90+ new Date().getMinutes() * 6)+'deg)'
     sec.style.transform = 'rotate('+(-90  +new Date().getSeconds() * 6)+'deg)'},100)
